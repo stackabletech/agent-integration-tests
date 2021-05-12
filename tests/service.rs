a85800878ca1e152791e188c58cf3a988a3a4f9e
Siegfried Weber <mail@siegfriedweber.net>
2021-05-12 13:36:43 +0200

    Test with restart after ungraceful shutdown added

--- tests/service.rs
+++ tests/service.rs
@@ -13,7 +13,7 @@ fn service_should_be_started_successfully() {
             apiVersion: v1
             kind: Pod
             metadata:
-              name: agent-service-integration-test
+              name: agent-service-integration-test-start
             spec:
               containers:
                 - name: noop-service
@@ -28,4 +28,33 @@ fn service_should_be_started_successfully() {
     );
 
     client.verify_pod_condition(&pod, "Ready");
+}
+
+#[test]
+fn restart_after_ungraceful_shutdown_should_succeed() {
+    let client = TestKubeClient::new();
+
+    setup_repository(&client);
+
+    let pod_spec = indoc! {"
+        apiVersion: v1
+        kind: Pod
+        metadata:
+          name: agent-service-integration-test-restart
+        spec:
+          containers:
+            - name: nostop-service
+              image: nostop-service:1.0.0
+              command:
+                - nostop-service-1.0.0/start.sh
+          tolerations:
+            - key: kubernetes.io/arch
+              operator: Equal
+              value: stackable-linux
+    "};
+
+    for _ in 1..=2 {
+        let pod = TemporaryResource::new(&client, pod_spec);
+        client.verify_pod_condition(&pod, "Ready");
+    }
 }
